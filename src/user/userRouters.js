986f6403ee8ecb43a54ca602d5548caa8b367cd8
horsellcommon <crawfordj716@gmail.com>
2022-10-24 15:24:54 +0100

    route file completed

---
+++ src/user/userRouters.js
@@ -0,0 +1,11 @@
+const { Router } = require("express");
+const { createUser, readUsers } = require("./userControllers");
+
+const userRouter = Router();
+
+userRouter.post("/create", createUser);
+userRouter.get("/read", readUsers);
+//update
+//delete
+
+module.exports = userRouter;
