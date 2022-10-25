77ff0401a10ce850c9980d3f454647ddcfb632af
horsellcommon <crawfordj716@gmail.com>
2022-10-25 11:10:39 +0100

    UD functions completed

--- src/user/userControllers.js
+++ src/user/userControllers.js
@@ -11,10 +11,34 @@ exports.createUser = async (req, res) => {
   }
 };
 
-exports.readUsers = async (req, res) => { //always need both arguments even if only one is used
+exports.readUsers = async (req, res) => {
+  //always need both arguments even if only one is used
   try {
     const users = await User.find({});
     res.status(200).send({ user: users });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error: error.message });
+  }
+};
+
+exports.updateUser = async (req, res) => {
+  try {
+    await User.updateOne(
+      { username: req.body.username },
+      { [req.body.key]: req.body.value }
+    );
+    res.status(201).send({ message: "Successfully updated user." });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error: error.message });
+  }
+};
+
+exports.deleteUser = async (req, res) => {
+  try {
+    await User.deleteOne({ username: req.params.username });
+    res.status(200).send({message: "User deleted."})
   } catch (error) {
     console.log(error);
     res.status(500).send({ error: error.message });
--- src/user/userRouters.js
+++ src/user/userRouters.js
@@ -1,11 +1,11 @@
 const { Router } = require("express");
-const { createUser, readUsers } = require("./userControllers");
+const { createUser, readUsers, updateUser, deleteUser } = require("./userControllers");
 
 const userRouter = Router();
 
 userRouter.post("/create", createUser);
 userRouter.get("/read", readUsers);
-//update
-//delete
+userRouter.put("/update", updateUser);
+userRouter.delete("/delete/:username", deleteUser);
 
 module.exports = userRouter;
