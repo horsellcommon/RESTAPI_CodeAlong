a343d4cee73da337745e6f5f472f68daf1d2f024
horsellcommon <crawfordj716@gmail.com>
2022-10-25 11:24:44 +0100

    Updated routers to include middleware

--- src/user/userRouters.js
+++ src/user/userRouters.js
@@ -1,9 +1,15 @@
 const { Router } = require("express");
-const { createUser, readUsers, updateUser, deleteUser } = require("./userControllers");
+const {
+  createUser,
+  readUsers,
+  updateUser,
+  deleteUser,
+} = require("./userControllers");
+const { hashPass } = require("../middleware");
 
 const userRouter = Router();
 
-userRouter.post("/create", createUser);
+userRouter.post("/create", hashPass, createUser);
 userRouter.get("/read", readUsers);
 userRouter.put("/update", updateUser);
 userRouter.delete("/delete/:username", deleteUser);
