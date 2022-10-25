fd4312f3c9cd16b62aef33ec08cf3d1b7ff1be0f
horsellcommon <crawfordj716@gmail.com>
2022-10-25 11:32:18 +0100

    loginUser added to userReouters

--- src/user/userRouters.js
+++ src/user/userRouters.js
@@ -4,12 +4,14 @@ const {
   readUsers,
   updateUser,
   deleteUser,
+  loginUser,
 } = require("./userControllers");
 const { hashPass } = require("../middleware");
 
 const userRouter = Router();
 
 userRouter.post("/create", hashPass, createUser);
+userRouter.post("/login", loginUser);
 userRouter.get("/read", readUsers);
 userRouter.put("/update", updateUser);
 userRouter.delete("/delete/:username", deleteUser);
